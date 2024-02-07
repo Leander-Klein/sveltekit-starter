e2b61f54724c2e85469003bf6ae442be03af4a6a
Leander-Klein <leander.klein@idmedia.com>
2024-02-07 14:00:55 +0100

    first

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,36 @@
 <script>
-  import Footer from '../lib/Footer.svelte'
-  import Nav from '../lib/Nav.svelte'
+  import { onMount } from 'svelte'
+  import axios from 'axios'
+
+  const spaceId = 'zcaut8zny79j'
+  const accessToken = 'barvv1Ke4ZD95chfJro2W4GVNLstiVDPLCidapyQZWA'
+  const apiUrl = `https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}`
+
+  let data = []
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(apiUrl)
+      data = response.data.items // Annehmen, dass Ihre Daten unter 'items' liegen
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Daten von Contentful:', error)
+    }
+  }
+  console.log(data)
+  onMount(() => {
+    fetchData()
+  })
 </script>
 
-<Nav />
-<main>
-  <slot />
-  <Footer />
-</main>
+<h1 class="text-bold underline bg-blue-500">Hallo</h1>
+<slot />
 
-<style global>
-  @import './global.css';
-</style>
+<main>
+  {#if data.length > 0}
+    {#each data as entry (entry.sys.id)}
+      <div>{entry.fields.title}</div>
+    {/each}
+  {:else}
+    <p>Lade Daten...</p>
+  {/if}
+</main>
