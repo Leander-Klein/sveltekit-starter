917e54a891f57b4ebe26a100a036cfa6edbedb3e
Leander-Klein <leander.klein@idmedia.com>
2024-02-08 08:15:05 +0100

    ghj

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script>
   import { onMount } from 'svelte'
   import axios from 'axios'
-  const { CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_SPACE_ID } = import.meta.env
-  const apiUrl = `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries?access_token=${CONTENTFUL_ACCESS_TOKEN}`
+  import { PUBLIC_CONTENTFUL_SPACE_ID, PUBLIC_CONTENTFUL_ACCESS_TOKEN } from '$env/static/public'
+  const apiUrl = `https://cdn.contentful.com/spaces/${PUBLIC_CONTENTFUL_SPACE_ID}/entries?access_token=${PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
 
   let data = []
 
   async function fetchData() {
     try {
       const response = await axios.get(apiUrl)
-      data = response.data.items // Annehmen, dass Ihre Daten unter 'items' liegen
+      data = response.data.items
+      console.log(data[0].fields.text)
     } catch (error) {
       console.error('Fehler beim Abrufen der Daten von Contentful:', error)
     }
@@ -26,6 +27,7 @@
   {#if data.length > 0}
     {#each data as entry (entry.sys.id)}
       <div>{entry.fields.title}</div>
+      <div>{entry.fields.text}</div>
     {/each}
   {:else}
     <p>Lade Daten...</p>
